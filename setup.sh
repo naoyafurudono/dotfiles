3aaef527645be15d2cccf42694e25931c5b45cb7
Naoya Furudono <naoyafurudono@gmail.com>
2023-08-17 00:04:50 +0900

    cargoでインストールするなど

--- setup.sh
+++ setup.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
-DONE='.~setup-done'
-
-
-if [ -f $DONE ]; then
-  echo "Already setup"
-  exit 0
-fi
-
 set -eu -o pipefail
+
+cp -rf dotfiles/.config "$HOME/.config"
 
 # branch by the os
 case "$(uname)" in
-  Darwin)
-    # Install Homebrew
-    # https://brew.sh/
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+Darwin)
+  # Install Homebrew
+  # https://brew.sh/
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-    brew install fish nvim bat fzf ripgrep fd
-    ;;
-  Linux)
-    sudo apt-get update -y
-    sudo apt-get install -y fish neovim bat fzf ripgrep fd-find
-    ;;
-  *)
-    echo "Unknown OS"
-    exit 1
-    ;;
+  brew install fish nvim fzf
+  ;;
+Linux)
+  set +e
+  apt-get update -y && apt-get install -y sudo
+  set -e
+  sudo apt-get update -y
+  sudo apt-get install -y fish neovim fzf curl
+  ;;
+*)
+  echo "Unknown OS" >&2
+  exit 1
+  ;;
 esac
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# shellcheck disable=SC1091
+source "$HOME/.cargo/env"
+cargo install exa bat fd-find ripgrep zoxide
 
 # Install asdf
 # https://asdf-vm.com/guide/getting-started.html
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 
 ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+touch tool-versions
 ln tool-versions "${HOME}/.tool-versions"
-
-touch $DONE

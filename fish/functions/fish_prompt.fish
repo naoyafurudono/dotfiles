424f667d5fe3a140fdbec01bfb463a6be3b4c4f2
Naoya Furudono <naoya-furudono@pepabo.com>
2023-08-31 10:21:30 +0900

    update

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -33,9 +33,9 @@ function fish_prompt
 
     set -l pwd (pwd)
 
-    set -l kube_prompt ""
-    if command -v kube_ps1 >/dev/null
-        set -l kube_prompt (__kube_prompt)
+    set kube_prompt ""
+    if type __kube_prompt >/dev/null
+        set kube_prompt (__kube_prompt)
     end
     echo -e -n -s $prompt_host $cwd $pwd $normal $prompt_status $kube_prompt '\n' $delim
 end

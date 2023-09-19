12e1f1dae52b1d75215ccee61fc218d9fb8bdb53
Naoya Furudono <naoyafurudono@gmail.com>
2023-09-19 23:52:07 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -76,7 +76,7 @@ if status --is-interactive
     fish_vi_key_bindings
 
     # Do after `fish_vi_key_bindings`, which overwrites follows
-    set -x fish_cursor_default line blink
+    set -x fish_cursor_default block
     set -x fish_cursor_insert line
     set -x fish_cursor_replace_one underscore
     set -x fish_cursor_visual block

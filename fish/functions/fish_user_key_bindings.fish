2586066eb6d8e445ddc42de071a42fad94e57809
Naoya Furudono <naoyafurudono@gmail.com>
2024-10-28 01:37:50 +0900

    update

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,12 @@
 function fish_user_key_bindings
   fzf_key_bindings
+  # fzf_key_bindingsの設定を上書きする
+  bind -e \cr
+  bind -M insert \cr fzf-sync-select-history
 end
+
+function fzf-sync-select-history
+    history merge
+    fzf-history-widget
+end
+

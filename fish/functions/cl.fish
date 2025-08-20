14b77708d1ef94f8a2f2c97e127fc3e9e9c7a021
Naoya Furudono <naoyafurudono@gmail.com>
2025-08-20 10:28:09 +0900

    clean repo

--- fish/functions/cl.fish
+++ fish/functions/cl.fish
@@ -1,5 +1,3 @@
-function cl -d 'fzfで選択したファイルと同じディレクトリ階層に第一引数で指定した名前のファイルを作成してコピーする'
-    set pathname (fzf --print0)
-    and set newname $argv[1]
-    and easy-cp $pathname $newname
+function cl -d 'git clean -fd && git reset --hard HEAD'
+    git clean -fd && git reset --hard HEAD
 end

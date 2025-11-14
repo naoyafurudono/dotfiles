d0c5296d981d06da32bca1f71ee5e9e2877afee4
Naoya Furudono <naoyafurudono@gmail.com>
2025-11-14 12:05:40 +0900

    Sync config

--- fish/functions/sconf.fish
+++ fish/functions/sconf.fish
@@ -1,3 +1,3 @@
 function sconf --description "Syncs the local config repository with the remote one"
-    _ (cd $XDG_CONFIG_HOME && git add -A && git commit -m "Sync config" && git pull || git push)
+    _ (cd $XDG_CONFIG_HOME && git add -A && git commit -m "Sync config" && git pull && git push)
 end

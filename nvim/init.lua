ce9c5fe335403edc1309dfd82da22543bc4c10c2
Naoya Furudono <naoyafurudono@gmail.com>
2026-01-06 12:27:12 +0900

    astro nvim を試す

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,27 @@
--- 一般的な設定
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
-vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { silent = true })
-vim.o.number = true
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if vim.fn.has('unix') == 1 then
-  vim.cmd [[
-    autocmd InsertLeavePre * :call system('ibus engine xkb:us::eng')
-  ]]
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
 end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+require "polish"

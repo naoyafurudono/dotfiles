68d064e1c505b128656d9c3900697f664f3c0dfb
Naoya Furudono <naoyafurudono@gmail.com>
2026-01-09 00:48:27 +0900

    plug-nvim

--- fish/config.fish
+++ fish/config.fish
@@ -53,13 +53,14 @@ if status --is-interactive
     abbr --add .... 'cd ../../../'
     abbr --add ..... 'cd ../../../../'
     abbr --add di diary
+    abbr --add h hustle
     abbr --add k kubectl
     abbr --add ka 'kubectl get (kubectl api-resources --namespaced=true --verbs=list -o name | tr "\n" "," | sed -e "s/,\$//")'
     abbr --add kp killport
     abbr --add l eza
+    abbr --add pn plug-nvim
     abbr --add rg 'rg --no-heading --smart-case'
     abbr --add t 'tree --gitignore'
-    abbr --add h hustle
 
     abbr --add a 'git add'
     abbr --add c 'git commit -m'

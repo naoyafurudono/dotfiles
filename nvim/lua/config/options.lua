4e1ae981c9a6a88e0dfa9650be7d51d56da50d1d
Naoya Furudono <naoyafurudono@gmail.com>
2026-01-09 01:46:42 +0900

    grepしたらcopenしたい

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,3 +9,11 @@ vim.opt.termguicolors = true
 -- ripgrepを:grepで使用
 vim.opt.grepprg = 'rg --vimgrep --smart-case'
 vim.opt.grepformat = '%f:%l:%c:%m'
+
+-- grep後に自動でquickfixを開く
+vim.api.nvim_create_autocmd('QuickFixCmdPost', {
+  pattern = 'grep',
+  callback = function()
+    vim.cmd('copen')
+  end,
+})

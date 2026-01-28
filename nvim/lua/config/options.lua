e4c7cae655e9aefb0fbce5bc6b06893ffd0f5cc4
Naoya Furudono <naoyafurudono@gmail.com>
2026-01-28 14:36:52 +0900

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -29,3 +29,27 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
+-- ファイル保存時に自動コミット＆プッシュ
+local auto_push_repos = {
+  ["donokun/memo"] = true,
+  ["naoyafurudono/dotfiles"] = true,
+}
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*",
+  callback = function()
+    local file_dir = vim.fn.expand("%:p:h")
+    local remote_url = vim.fn.trim(vim.fn.system("cd " .. vim.fn.shellescape(file_dir) .. " && git remote get-url origin 2>/dev/null"))
+    if vim.v.shell_error ~= 0 or remote_url == "" then
+      return
+    end
+    -- git@github.com:owner/repo.git or https://github.com/owner/repo.git -> owner/repo
+    local repo = remote_url:gsub("^git@[^:]+:", ""):gsub("^https?://[^/]+/", ""):gsub("%.git$", "")
+    if auto_push_repos[repo] then
+      vim.fn.jobstart(
+        "cd " .. vim.fn.shellescape(file_dir) .. " && git add -A && git commit -m update && git push",
+        { detach = true }
+      )
+    end
+  end,
+})
